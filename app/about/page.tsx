6f5ef041b8b0aee599a46aa77c22f8d32fc17690
priyanshu lohar <priyanshulohar1@gmail.com>
2024-05-26 01:17:43 +0530

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,19 +6,19 @@ const About = () => {
   <div className="relative pt-6 pb-16 sm:pb-24">
     <p className="text-4xl px-8 py-8 font-bold text-center text-brown-800">About Me</p>
     <p className="text-2xl px-8 py-8 text-left sm:px-12 lg:px-32 xl:px-48 text-brown-800">🌟 Welcome to my corner of the digital world!</p>
-    <p className="text-lg px-8 py-1 text-left sm:px-12 lg:px-32 xl:px-48 leading-relaxed text-brown-700">I'm Priyanshu Lohar, a passionate explorer, creator, and connector.</p>
+    <p className="text-lg px-8 py-1 text-left sm:px-12 lg:px-32 xl:px-48 leading-relaxed text-brown-700">I&aposm Priyanshu Lohar, a passionate explorer, creator, and connector.</p>
     <p className="text-lg px-8 py-1 text-left sm:px-12 lg:px-32 xl:px-48 leading-relaxed text-brown-700">✨ Enthusiast of literature, music, and technology.</p>
     <p className="text-lg px-8 py-1 text-left sm:px-12 lg:px-32 xl:px-48 leading-relaxed text-brown-700">📚 Avid reader of non-fiction, fantasy, and classics.</p>
     <p className="text-lg px-8 py-1 text-left sm:px-12 lg:px-32 xl:px-48 leading-relaxed text-brown-700">📝 Expresses thoughts through poetry and prose.</p>
     <p className="text-lg px-8 py-1 text-left sm:px-12 lg:px-32 xl:px-48 leading-relaxed text-brown-700">🎶 Diverse musical tastes from Sufi to Celtic tunes.</p>
-    <p className="text-lg px-8 py-1 text-left sm:px-12 lg:px-32 xl:px-48 leading-relaxed text-brown-700">🌊 Finds solace in Enya's ethereal compositions.</p>
+    <p className="text-lg px-8 py-1 text-left sm:px-12 lg:px-32 xl:px-48 leading-relaxed text-brown-700">🌊 Finds solace in Enya&aposs ethereal compositions.</p>
     <p className="text-lg px-8 py-1 text-left sm:px-12 lg:px-32 xl:px-48 leading-relaxed text-brown-700">💻 Engineer with a flair for innovation and problem-solving.</p>
     <p className="text-lg px-8 py-1 text-left sm:px-12 lg:px-32 xl:px-48 leading-relaxed text-brown-700">🔍 Skilled in software development, data science, and machine learning.</p>
     <p className="text-lg px-8 py-1 text-left sm:px-12 lg:px-32 xl:px-48 leading-relaxed text-brown-700">🌿 Nature lover, rejuvenated by the outdoors.</p>
     <p className="text-lg px-8 py-1 text-left sm:px-12 lg:px-32 xl:px-48 leading-relaxed text-brown-700">🌄 Enjoys hiking and quiet moments by the sea.</p>
     <p className="text-lg px-8 py-1 text-left sm:px-12 lg:px-32 xl:px-48 leading-relaxed text-brown-700">🚀 Driven by curiosity and optimism.</p>
     <p className="text-lg px-8 py-1 text-left sm:px-12 lg:px-32 xl:px-48 leading-relaxed text-brown-700">🌟 Lifelong learner and dreamer, eager to explore new horizons.</p>
-    <p className="text-xl px-8 py-8 font-bold text-center text-brown-800">Thank you for visiting! Let's discover the magic around us together.</p>
+    <p className="text-xl px-8 py-8 font-bold text-center text-brown-800">Thank you for visiting! Let&aposs discover the magic around us together.</p>
   </div>
 </div>
   )
