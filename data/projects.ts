d5cfc5fa15508df8a1c127a99c975c1f8378eb09
priyanshu lohar <priyanshulohar1@gmail.com>
2024-05-31 00:32:54 +0530

    project - portfolio added

--- data/projects.ts
+++ data/projects.ts
@@ -17,11 +17,11 @@ export interface Project {
       image: "/project-images/flex-movie.png" // Path to your project image
     },
     {
-      name: "Project 2",
-      description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.",
-      techStack: "Vue.js, Express, PostgreSQL",
-      liveLink: "https://example.com/project2",
-      image: "/images/project2.jpg" // Path to your project image
+      name: "PYLHR - Priyanshu's Portfolio",
+      description: "This is my personal portfolio and my digital sanctum.",
+      techStack: "React, Next.js, Tailwind CSS, Remark, TypeScript",
+      liveLink: "https://pylhr.vercel.app",
+      image: "/project-images/portfolio.png" // Path to your project image
     },
     {
         name: "Project 3",
