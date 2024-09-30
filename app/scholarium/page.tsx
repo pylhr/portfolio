660980ccaed13c6b816575a9018a2a68b24c7b75
priyanshu lohar <priyanshulohar1@gmail.com>
2024-09-30 23:39:13 +0530

    Update page.tsx

--- app/scholarium/page.tsx
+++ app/scholarium/page.tsx
@@ -6,7 +6,7 @@ const Scholarium = () => {
       <div className="relative pt-2 pb-16 sm:pb-24">
         <section className="mx-auto w-11/12 lg:w-2/3 mt-20">
           <h1 className="text-4xl font-bold text-center text-gray-800 mb-10">
-            Priyanshu's Scholarium
+            Priyanshu&aspos;s Scholarium
           </h1>
           
         </section>
