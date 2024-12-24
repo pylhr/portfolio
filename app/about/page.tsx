0553954ca72c7869b518beb595ac32f767783a64
priyanshu lohar <priyanshulohar1@gmail.com>
2024-12-25 03:21:25 +0530

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,8 +5,8 @@ const About = () => {
     <div className="bg-beige-50 text-brown-900 px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
   <div className="relative pt-6 pb-16 sm:pb-24">
     <p className="text-4xl px-8 py-8 font-bold text-center text-brown-800">About Me</p>
-    <p className="text-2xl px-8 py-8 text-left sm:px-12 lg:px-32 xl:px-48 text-brown-800">I’m Priyanshu Lohar, a software engineer and entrepreneur with a passion for artificial intelligence, machine learning, and software development.</p>
-    <p className="text-lg px-8 py-1 text-left sm:px-12 lg:px-32 xl:px-48 leading-relaxed text-brown-700">Currently, I lead Aquilonis AI, a firm creating AI-driven automation tools, and Elyon Aquaris, a fintech startup focused on building personalized investment products. I also explore quantitative research and advanced AI systems through Priyanshu's Scholarium, my personal research hub.</p>
+    <p className="text-2xl px-8 py-8 text-left sm:px-12 lg:px-32 xl:px-48 text-brown-800">I&apos;m Priyanshu Lohar, a software engineer and entrepreneur with a passion for artificial intelligence, machine learning, and software development.</p>
+    <p className="text-lg px-8 py-1 text-left sm:px-12 lg:px-32 xl:px-48 leading-relaxed text-brown-700">Currently, I lead Aquilonis AI, a firm creating AI-driven automation tools, and Elyon Aquaris, a fintech startup focused on building personalized investment products. I also explore quantitative research and advanced AI systems through Priyanshu&apos;s Scholarium, my personal research hub.</p>
     <p className="text-lg px-8 py-1 text-left sm:px-12 lg:px-32 xl:px-48 leading-relaxed text-brown-700">I specialize in AI solutions, data science, quantitative analysis, and full-stack development, with a focus on building industry-ready software using technologies like C++, Python, Java, Golang, TensorFlow, PyTorch, and more.</p>
     <p className="text-xl px-8 py-8 font-bold text-center text-brown-800">Thank you for visiting!</p>
 </div>
