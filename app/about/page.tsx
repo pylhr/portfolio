a4d224a8cba295e8e8cee57af755a860ae8cbd25
priyanshu lohar <priyanshulohar1@gmail.com>
2024-12-25 03:20:04 +0530

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,7 +7,7 @@ const About = () => {
     <p className="text-4xl px-8 py-8 font-bold text-center text-brown-800">About Me</p>
     <p className="text-2xl px-8 py-8 text-left sm:px-12 lg:px-32 xl:px-48 text-brown-800">I’m Priyanshu Lohar, a software engineer and entrepreneur with a passion for artificial intelligence, machine learning, and software development.</p>
     <p className="text-lg px-8 py-1 text-left sm:px-12 lg:px-32 xl:px-48 leading-relaxed text-brown-700">Currently, I lead Aquilonis AI, a firm creating AI-driven automation tools, and Elyon Aquaris, a fintech startup focused on building personalized investment products. I also explore quantitative research and advanced AI systems through Priyanshu's Scholarium, my personal research hub.</p>
-    <p className="text-lg px-8 py-1 text-left sm:px-12 lg:px-32 xl:px-48 leading-relaxed text-brown-700">I specialize in AI solutions, data science, quantitative analysis, and full-stack development, with a focus on building industry-ready software using technologies like Python, Java, Golang, TensorFlow, PyTorch, and more.</p>
+    <p className="text-lg px-8 py-1 text-left sm:px-12 lg:px-32 xl:px-48 leading-relaxed text-brown-700">I specialize in AI solutions, data science, quantitative analysis, and full-stack development, with a focus on building industry-ready software using technologies like C++, Python, Java, Golang, TensorFlow, PyTorch, and more.</p>
     <p className="text-xl px-8 py-8 font-bold text-center text-brown-800">Thank you for visiting!</p>
 </div>
 
